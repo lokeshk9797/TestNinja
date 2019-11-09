740f7bc7358d364042f3e928d66daaec3b671d0d
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-11-09 12:54:37 +0530

    Reservation Tests Using NUint

---
+++ TestNinja.UnitTests/ReservationTests_NUnit.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+
+    [TestFixture]
+    public class ReservationTests_NUints
+    {
+        [Test]
+        public void CanBeCancelled_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange 
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert - Method 1
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void CanBeCancelled_SameUserCancels_ReturnsTrue()
+        {
+            //Arrange 
+            var user = new User() { IsAdmin = false };
+            var reservation = new Reservation() { MadeBy = user };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert - Method 2
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelled_DiffrentUserCancels_ReturnsFalse()
+        {
+            //Arrange 
+            var reservation = new Reservation() { MadeBy = new User() };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert - Method 3
+            Assert.That(result == false);
+        }
+
+
+
+    }
+}
