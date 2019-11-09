f371ff36b801ddccdc4e30644f9b003b149d3543
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-11-09 12:44:26 +0530

    Reservation Tests in MSTests

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelled_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange 
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void CanBeCancelled_SameUserCancels_ReturnsTrue()
+        {
+            //Arrange 
+            var user = new User() { IsAdmin = false };
+            var reservation = new Reservation() { MadeBy = user };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelled_DiffrentUserCancels_ReturnsFalse()
+        {
+            //Arrange 
+            var reservation = new Reservation() { MadeBy = new User() };
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+
+
+    }
+}
