503435b49a2742979b990012447bc67c87d23062
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-11-13 15:36:22 +0530

    HtmlFormatterTests (String Tests)

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            //When case need to be ignored
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase); 
+
+
+            //More General
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+
+
+        }
+
+
+    }
+}
