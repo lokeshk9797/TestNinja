592b6a630b3b6158e8c5c5d5c4dab791a82f27d9
Lokesh Kashyap <lokeshkashyap858@gmail.com>
2019-11-26 12:35:12 +0530

    OrderService Tests Implemented

---
+++ TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheObject()
+        {
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+            var order = new Order();
+            orderService.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
